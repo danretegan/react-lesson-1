fe35fc04b80cac908bd042f90969fa061e4f46db
Dan <danretegan@yahoo.com>
2024-02-16 08:49:58 +0200

    make Menu.jsx component
    My first component!

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Menu from './Menu';
 
 function App() {
 
@@ -22,9 +23,9 @@ const menuItems = [
         <li>{menuItems[1].name}</li>
         <li>{menuItems[2].name}</li>
       </ul>
-    <p>Acesta este primul meu text in React!</p>
-    <button onClick={clickButton}>Click button 1!</button>
-    <button onClick={() => {console.log('button 2 clicked')}}>Click button 2!</button>
+      <Menu/>
+      <button onClick={clickButton}>Click button 1!</button>
+      <button onClick={() => {console.log('button 2 clicked')}}>Click button 2!</button>
     </div>
   )
 }
---
+++ my-app/src/Menu.jsx
@@ -0,0 +1,9 @@
+const Menu = () => {
+    return (
+        <div>
+           <p>Acesta este primul meu text in React!</p>
+        </div>
+    )
+}
+
+export default Menu;
