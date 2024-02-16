a16fc5d2290c403316e5413ea80377f2cc1e2999
Dan <danretegan@yahoo.com>
2024-02-16 08:00:20 +0200

    add title using a variable

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 
 function App() {
-  
+
+const title = 'React'
 
   return (
     <div>
+      <h1>{title}</h1>
     <p>Acesta este primul meu text in React!</p>
     </div>
   )
