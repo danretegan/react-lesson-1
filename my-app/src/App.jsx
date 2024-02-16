18ea41780af3e11a74bf7c7aca6dc41954cfd79b
Dan <danretegan@yahoo.com>
2024-02-16 08:37:20 +0200

    add a menu

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -8,9 +8,20 @@ const clickButton = () => {
   console.log('button 1 clicked');
 }
 
+const menuItems = [
+  { name: 'Home' },
+  { name: 'About' },
+  { name: 'Contacts' },
+]
+
   return (
     <div>
       <h1>{title}</h1>
+      <ul>
+        <li>{menuItems[0].name}</li>
+        <li>{menuItems[1].name}</li>
+        <li>{menuItems[2].name}</li>
+      </ul>
     <p>Acesta este primul meu text in React!</p>
     <button onClick={clickButton}>Click button 1!</button>
     <button onClick={() => {console.log('button 2 clicked')}}>Click button 2!</button>
