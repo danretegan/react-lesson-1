ab8b907775461d60c30610df30cabfdc7d68af19
Dan <danretegan@yahoo.com>
2024-02-16 08:10:16 +0200

    add 2 buttons

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,10 +4,16 @@ function App() {
 
 const title = 'React'
 
+const clickButton = () => {
+  console.log('button 1 clicked');
+}
+
   return (
     <div>
       <h1>{title}</h1>
     <p>Acesta este primul meu text in React!</p>
+    <button onClick={clickButton}>Click button 1!</button>
+    <button onClick={() => {console.log('button 2 clicked')}}>Click button 2!</button>
     </div>
   )
 }
